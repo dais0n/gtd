f585ceeacc4b6ae69f416250d024b0477f6e5468
dais0n <t.omura8383@gmail.com>
2017-03-08 22:45:33 +0900

    add func with Todo.Children

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	mapset "github.com/deckarep/golang-set"
 	"github.com/urfave/cli"
@@ -16,14 +14,18 @@ import (
 
 var Version = "0.0.1"
 
-const checkmark = "\u2714 "
+const (
+	todo_mark      = "\u2610 "
+	todo_done_mark = "\u2611 "
+)
 
 type Todo struct {
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
-	Tag   string `json:"tag"`
-	Date  string `json:"date"`
-	Memo  string `json:"memo"`
+	Title    string `json:"title"`
+	Done     bool   `json:"done"`
+	Tag      string `json:"tag"`
+	Date     string `json:"date"`
+	Memo     string `json:"memo"`
+	Children []Todo `json:"children"`
 }
 
 func main() {
@@ -50,11 +52,7 @@ func Run(args []string) int {
 				},
 				cli.StringFlag{
 					Name:  "tag, t",
-					Usage: "add `TAG` to todo (ex: gtd add -t memo task)",
-				},
-			},
-		},
-		{
+					Usage: "add `TAG` to todo (ex: gtd add -t memo task)"}}}, {
 			Name:    "list",
 			Aliases: []string{"l"},
 			Usage:   "list todo",
@@ -92,76 +90,69 @@ func Run(args []string) int {
 func addTodoAction(c *cli.Context) error {
 	var title string
 	var tag string
+	var parent string
 	var todos []Todo
 
 	todos, err := todoRead("/var/tmp/todo.json")
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to read todofile: %v", err)
 	}
 
 	if c.Args().Present() {
 		title = c.Args().First()
+		tag = c.String("tag")
+		parent = c.String("parent")
 	} else {
 		cli.ShowCommandHelp(c, "add")
-		return errors.New("Failed to parse options")
+		return fmt.Errorf("Failed to parse options")
 	}
 
-	tag = c.String("tag")
-	date := time.Now().Format("2006-01-02")
-	todo := Todo{Title: title, Done: false, Date: date, Tag: tag}
-	todos = append(todos, todo)
+	todo := Todo{Title: title, Done: false, Tag: tag}
 
-	if err := todoWrite("/var/tmp/todo.json", todos); err != nil {
-		return err
-	}
+	parentnums, err := parseTodoNum(parent)
+	todos, err = appendTodo(todos, todo, parentnums)
 
-	return nil
+	return todoWrite("/var/tmp/todo.json", todos)
 }
 
 func listTodoAction(c *cli.Context) error {
 	var todos []Todo
+
 	todos, err := todoRead("/var/tmp/todo.json")
 	if err != nil {
 		return err
 	}
+
+	fmt.Println("\u2705  YOUR TO DO LIST")
+
 	if c.Bool("all") {
-		for i, v := range todos {
-			if v.Done {
-				fmt.Print(checkmark)
-			}
-			fmt.Printf("%v: %v: %v \n", i, v.Title, v.Date)
-		}
-	} else {
-		for i, v := range todos {
-			if !v.Done {
-				fmt.Printf("%v: %v: %v \n", i, v.Title, v.Date)
-			}
-		}
+		displayAllTodo(todos, "")
+		return nil
 	}
+
+	displayTodo(todos, "")
 	return nil
 }
 
 func doneTodoAction(c *cli.Context) error {
-	var todonum int
 	var todos []Todo
-	if c.Args().Present() {
-		donenum, err := strconv.Atoi(c.Args().First())
-		if err != nil {
-			return err
-		}
-		todonum = donenum
-	} else {
+
+	if !c.Args().Present() {
 		return fmt.Errorf("Failed to parse options")
 	}
+
 	todos, err := todoRead("/var/tmp/todo.json")
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to read jsonfile: %v")
 	}
-	for i, _ := range todos {
-		if todonum == i {
-			todos[i].Done = true
-		}
+
+	todonum := c.Args().First()
+	todonumlist, err := parseTodoNum(todonum)
+	todos, err = doneTodo(todos, todonumlist)
+	if err != nil {
+		return fmt.Errorf("Failed to read jsonfile: %v", err)
 	}
+
 	if err := todoWrite("/var/tmp/todo.json", todos); err != nil {
 		return err
 	}
@@ -171,6 +162,7 @@ func doneTodoAction(c *cli.Context) error {
 
 func tagTodoAction(c *cli.Context) error {
 	var todos []Todo
+
 	todos, err := todoRead("/var/tmp/todo.json")
 	if err != nil {
 		return err
@@ -178,28 +170,28 @@ func tagTodoAction(c *cli.Context) error {
 
 	if c.Bool("all") {
 		var tags = mapset.NewSet()
-
-		for _, v := range todos {
-			if v.Tag != "" {
-				tags.Add(v.Tag)
-			}
-		}
-		for tag := range tags.Iter() {
-			fmt.Println(tag)
-		}
-		return nil
+		displayAllTags(todos, tags)
 	} else if tag := c.Args().First(); tag != "" {
-		for i, v := range todos {
-			if v.Tag == tag {
-				fmt.Printf("%v: %v: %v \n", i, v.Title, v.Date)
-			}
-		}
+		displayTagTodo(todos, tag)
 	} else {
 		cli.ShowCommandHelp(c, "list")
 		return fmt.Errorf("Failed to parse options")
 	}
 
 	return nil
+}
+
+// parse "1.1.0" to [1,1,0]
+func parseTodoNum(todonum string) ([]int, error) {
+	var todonums []int
+	for _, v := range strings.Split(todonum, ".") {
+		if id, err := strconv.Atoi(v); err != nil {
+			return nil, err
+		} else {
+			todonums = append(todonums, id)
+		}
+	}
+	return todonums, nil
 }
 
 func todoWrite(path string, todos []Todo) error {
@@ -221,5 +213,80 @@ func todoRead(path string) ([]Todo, error) {
 			return nil, fmt.Errorf("Failed to decode data %v", err)
 		}
 		return todos, nil
+	}
+}
+
+func appendTodo(todos []Todo, todo Todo, todonum []int) ([]Todo, error) {
+	if len(todonum) == 0 {
+		return append(todos, todo), nil
+	}
+	if len(todonum) == 1 {
+		todos[todonum[0]].Children = append(todos[todonum[0]].Children, todo)
+		return todos, nil
+	} else {
+		todos_children, _ := appendTodo(todos[todonum[0]].Children, todo, todonum[1:])
+		todos[todonum[0]].Children = todos_children
+		return todos, nil
+	}
+}
+
+func displayTodo(todos []Todo, tab string) {
+	for id, todo := range todos {
+		if todo.Done {
+			continue
+		}
+		fmt.Print(tab, todo_mark)
+		fmt.Printf("%v: %v: %v (%v)\n", id, todo.Title, todo.Date, todo.Tag)
+		if todo.Children != nil {
+			displayTodo(todo.Children, tab+" ")
+		}
+	}
+}
+
+func displayAllTodo(todos []Todo, tab string) {
+	for id, todo := range todos {
+		if todo.Done {
+			fmt.Print(tab, todo_done_mark)
+		} else {
+			fmt.Print(tab, todo_mark)
+		}
+		fmt.Printf("%v: %v: %v (%v)\n", id, todo.Title, todo.Date, todo.Tag)
+		if todo.Children != nil {
+			displayAllTodo(todo.Children, tab+" ")
+		}
+	}
+}
+
+func doneTodo(todos []Todo, todonum []int) ([]Todo, error) {
+	if len(todonum) == 1 {
+		todos[todonum[0]].Done = true
+		return todos, nil
+	} else {
+		todos_children, _ := doneTodo(todos[todonum[0]].Children, todonum[1:])
+		todos[todonum[0]].Children = todos_children
+		return todos, nil
+	}
+}
+
+func displayTagTodo(todos []Todo, tag string) {
+	for id, todo := range todos {
+		if todo.Tag == tag {
+			fmt.Printf("%v: %v: %v (%v)\n", id, todo.Title, todo.Date, todo.Tag)
+		}
+		if todo.Children != nil {
+			displayTagTodo(todo.Children, tag)
+		}
+	}
+}
+
+func displayAllTags(todos []Todo, tags mapset.Set) {
+	for _, todo := range todos {
+		add := tags.Add(todo.Tag)
+		if todo.Tag != "" && add {
+			fmt.Println(todo.Tag)
+		}
+		if todo.Children != nil {
+			displayAllTags(todo.Children, tags)
+		}
 	}
 }
